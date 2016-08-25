ed26e5d57ff9fe137f55f7b189634b96f18ec570
sashank6 <sashank.alladi@live.com>
2016-08-25 17:17:44 +0530

    Completed UI Design

--- GadiyaCalendarApp/src/main/java/Main.java
+++ GadiyaCalendarApp/src/main/java/Main.java
@@ -1,6 +1,9 @@
 import java.io.IOException;
 import java.util.List;
 
+import javax.swing.JFrame;
+
+import com.gadiyacalendar.ui.UI;
 import com.gadiyacalendarapp.core.Service;
 import com.google.api.client.util.DateTime;
 import com.google.api.services.calendar.model.Event;
@@ -15,7 +18,12 @@ public class Main {
 
 		Service service=new Service();
 		com.google.api.services.calendar.Calendar calservice = service.getCalendarService();
-		service.addEvent("TestEvent", "Hyderabad", "Test", System.currentTimeMillis(), 3);
+//		service.addEvent("TestEvent", "Hyderabad", "Test", System.currentTimeMillis(), 3);
+		UI ui=new UI();
+		ui.setTitle("Gadiya Calendar");
+		ui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		ui.setVisible(true);
+		ui.setSize(500, 300);
 	}
 
 }
--- GadiyaCalendarApp/src/main/java/com/gadiyacalendarapp/core/Service.java
+++ GadiyaCalendarApp/src/main/java/com/gadiyacalendarapp/core/Service.java
@@ -7,9 +7,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Calendar;
 import java.util.List;
-import java.util.TimeZone;
 
 /**
  * @author SashankAlladi
